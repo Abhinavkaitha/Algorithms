c97904dde7dba89caf6431bcc8702ae354489b9d
Abhinav Reddy Kaitha <kaitha.abhi@gmail.com>
2020-02-23 11:33:51 -0800

    new_files

--- BInary Tree.py
+++ BInary Tree.py
@@ -1,9 +1,67 @@
 class Node:
-
     def __init__(self,val):
         self.value = val
         self.left = None
         self.right = None
+
+
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+
+
+
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def __str__(self):
+        s = ""
+        for i in range(len(self.items)):
+            s += str(self.items[i].value) + "-"
+        return s
 
 class Tree:
 
@@ -17,6 +75,10 @@ class Tree:
             return self.in_order_print(tree.root, "")
         elif traversal_type == 'postorder':
             return self.postorder_print(tree.root, "")
+        elif traversal_type == 'levelorder':
+            return self.level_order_print(tree.root)
+        elif traversal_type == 'reverselevelorder':
+            return self.reverse_level_order(tree.root)
 
     def pre_order_print(self, start, traversal):
         if start:
@@ -39,14 +101,55 @@ class Tree:
             traversal += (str(start.value) + "-")
         return traversal
 
+    def level_order_print(self, start):
+        if start is None:
+            return
+        queue = Queue()
+        queue.enqueue(start)
 
-    # def insert(self, data):
-    #
-    #     if self.root:
-    #         return self.root.insert(data)
-    #     else:
-    #         self.root = Node(data)
-    #         return True
+        traversal = ""
+
+        while len(queue)>0:
+
+            current_node = queue.dequeue()
+            traversal = traversal + '-' + str(current_node.value)
+
+            if current_node.left:
+                queue.enqueue(current_node.left)
+            if current_node.right:
+                queue.enqueue(current_node.right)
+        return traversal
+
+    def reverse_level_order(self, start):
+
+        if start is None:
+            return
+        queue = Queue()
+        stack = Stack()
+
+        queue.enqueue(start)
+        traversal = ""
+
+        while len(queue) > 0:
+            current_node = queue.dequeue()
+            stack.push(current_node)
+            if current_node.right:
+                queue.enqueue(current_node.right)
+            if current_node.left:
+                queue.enqueue(current_node.left)
+        while len(stack) > 0:
+            node = stack.pop()
+            traversal = traversal+str(node.value)+"-"
+        return traversal
+
+    def height(self,node):
+
+        if not node:
+            return -1
+        L = self.height(node.left)
+        R = self.height(node.right)
+
+        return 1+max(L,R)
 
 # 1-2-4-5-3-6-7-
 # 4-2-5-1-6-3-7
@@ -66,7 +169,10 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
-print(tree.print_tree("preorder"))
-print(tree.print_tree("inorder"))
-print(tree.print_tree("postorder"))
+print('Pre Order ',tree.print_tree("preorder"))
+print('In Order ', tree.print_tree("inorder"))
+print('Post Order ', tree.print_tree("postorder"))
+print('Level Order ', tree.print_tree('levelorder'))
+print('Reverse Level Order ', tree.print_tree('reverselevelorder'))
+print(tree.height(tree.root))
 
